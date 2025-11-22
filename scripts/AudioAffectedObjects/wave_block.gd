329b48cad10b03bdd84d30439ebc9c1f99a77a71
Nat10k <156871636+NoArt278@users.noreply.github.com>
2025-11-22 22:06:09 +0700

    fix : change wave block target to vector2 pos

--- scripts/AudioAffectedObjects/wave_block.gd
+++ scripts/AudioAffectedObjects/wave_block.gd
@@ -3,15 +3,12 @@ extends AudioAffectedObject
 class_name WaveBlock
 
 @export var block_frequency_range : AudioRange
-@export var target_height : float
+@export var target_position : Vector2
 @onready var particles: CPUParticles2D = $Particles
-var spawn_height : float
-var facing_downward : bool = false
+var spawn_position : Vector2
 
 func _ready() -> void:
-	spawn_height = position.y
-	if spawn_height < target_height : 
-		facing_downward = true
+	spawn_position = position
 	particles.color = Globals.wave_manager.block_color_gradient.get_color(block_frequency_range)
 
 func audio_effect(delta : float = 1) -> void:
@@ -19,23 +16,11 @@ func audio_effect(delta : float = 1) -> void:
 	var min_freq : float = Globals.get_min_frequency() + (global_freq_range * block_frequency_range/ AudioRange.size())
 	var max_freq : float = Globals.get_min_frequency() + (global_freq_range * (block_frequency_range + 1) / AudioRange.size())
 	var curr_energy : float = Globals.wave_manager.get_audio_energy(min_freq, max_freq)
-	var curr_height : float
 	
-	if facing_downward :
-		curr_height = target_height - curr_energy * target_height
-		if curr_height < spawn_height :
-			curr_height = spawn_height
-		elif curr_height > target_height :
-			curr_height = target_height
+	if curr_energy > 0 :
+		position = position.lerp(target_position, delta * curr_energy)
 	else :
-		curr_height = spawn_height - curr_energy * spawn_height
-		if curr_height > spawn_height :
-			curr_height = spawn_height
-		elif curr_height < target_height :
-			curr_height = target_height
-	
-	position.y = lerpf(position.y, curr_height, delta * 
-		smoothstep(min(curr_height, position.y), max(spawn_height, position.y), max(spawn_height, position.y)))
+		position = position.lerp(spawn_position, delta * 0.4)
 
 func _physics_process(delta: float) -> void:
 	audio_effect(delta)
