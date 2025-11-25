45cb506ce558e21aac78006bf67d234dd3df31e8
Nat10k <156871636+NoArt278@users.noreply.github.com>
2025-11-25 19:26:13 +0700

    fix : divide keyboard audio output to 10 ranges

--- scripts/audio_player.gd
+++ scripts/audio_player.gd
@@ -2,7 +2,6 @@ extends AudioStreamPlayer2D
 
 var playback : AudioStreamPlayback
 @onready var sample_hz : float = stream.mix_rate
-var pulse_hz : float
 var phase : float = 0.0
 
 func _ready() -> void:
@@ -11,7 +10,7 @@ func _ready() -> void:
 
 func fill_buffer(frequency_range : float) -> void:
 	var global_freq_range = Globals.get_max_frequency() - Globals.get_min_frequency()
-	var frequency : float = Globals.get_min_frequency() + global_freq_range * frequency_range / AudioAffectedObject.AudioRange.size()
+	var frequency : float = Globals.get_min_frequency() + global_freq_range * frequency_range
 	var increment = frequency / sample_hz
 	var frames_available = playback.get_frames_available()
 	
@@ -20,26 +19,19 @@ func fill_buffer(frequency_range : float) -> void:
 		phase = fmod(phase + increment, 1.0)
 
 func _process(_delta: float) -> void:
-	var curr_audio_range : float = -1
-	var active_key_press : int = 0
-	if Input.is_action_pressed("low_sound") :
-		curr_audio_range = AudioAffectedObject.AudioRange.LOW
-		active_key_press += 1
-	if Input.is_action_pressed("medium_sound") :
-		if curr_audio_range < 0 :
-			curr_audio_range = AudioAffectedObject.AudioRange.MEDIUM
-		else :
-			curr_audio_range += AudioAffectedObject.AudioRange.MEDIUM
-		active_key_press += 1
-	if Input.is_action_pressed("high_sound") :
-		if curr_audio_range < 0 :
-			curr_audio_range = AudioAffectedObject.AudioRange.HIGH
-		else :
-			curr_audio_range += AudioAffectedObject.AudioRange.HIGH
-		active_key_press += 1
+	# Divide frequency range into 10
+	var curr_audio_range : float = 0
+	var active_key_press_count : int = 0
 	
-	if active_key_press > 0 :
-		curr_audio_range /= active_key_press
-		fill_buffer(curr_audio_range)
+	for i : int in range(10):
+		if Input.is_key_pressed(KEY_0 + i):
+			if i == 0 :
+				curr_audio_range += 10
+			else :
+				curr_audio_range += i
+			active_key_press_count += 1
+	if active_key_press_count > 0 :
+		curr_audio_range /= active_key_press_count
+		fill_buffer(curr_audio_range / 10)
 	else :
 		phase = 0.0
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,4 +7,7 @@ func _on_settings_button_pressed() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
-		settings.open_settings(null)
+		if settings.visible :
+			settings.close_settings()
+		else :
+			settings.open_settings(null)
