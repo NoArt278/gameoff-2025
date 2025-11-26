a141b5bba16c9f22ea90f6694d096928af53e506
Nat10k <156871636+NoArt278@users.noreply.github.com>
2025-11-26 21:57:28 +0700

    feat : ui text noise shader

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var settings: Setting = $UnaffectedByVisibility/Settings
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+	get_tree().change_scene_to_file("res://scenes/UI/level_select.tscn")
 
 
 func _on_settings_button_pressed() -> void:
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -61,4 +61,4 @@ func _on_back_button_pressed() -> void:
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
