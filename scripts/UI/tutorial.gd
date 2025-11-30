cc68c22f3f026298f7f12ba81fd567d4d92ddecd
Nat10k <156871636+NoArt278@users.noreply.github.com>
2025-11-30 11:05:34 +0700

    fix : use touch screen button for mobile controls

--- scripts/UI/tutorial.gd
+++ scripts/UI/tutorial.gd
@@ -1,6 +1,13 @@
 extends Control
 
 @export var prev_screen : Control
+@onready var controls_keyboard: Label = $ControlsKeyboard
+@onready var controls_mobile: Label = $ControlsMobile
+
+func _ready() -> void:
+	if OS.has_feature("web_android") or OS.has_feature("web_ios") :
+		controls_keyboard.visible = false
+		controls_mobile.visible = true
 
 func close_tutorial() -> void:
 	visible = false
